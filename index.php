33f8441544346c9d41905f1f42e32e71bfd46e4e
yazidkhan96 <mryazidkhan@gmail.com>
2019-10-07 05:02:33 +0700

    menambahkan h1

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 <body>
 	<h1>Hello WORLD!</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus tempore pariatur quisquam beatae debitis. Iusto aliquam inventore, rem odit quos non fugit maiores illo delectus, labore neque praesentium saepe, tempora.</p>
+	<h1>Halo</h1>
 </body>
 </html>
